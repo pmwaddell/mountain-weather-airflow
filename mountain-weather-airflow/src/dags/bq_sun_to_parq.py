bef71c563b90ec524a16093d1be2413a3c2967e6
pmwaddell <pmwaddell9@gmail.com>
2024-07-26 15:56:52 -0400

    Add Airflow pipelines to save forecast and sun BQ staging tables as local parquet files

--- mountain-weather-airflow/src/dags/bq_sun_to_parq.py
+++ mountain-weather-airflow/src/dags/bq_sun_to_parq.py
@@ -14,15 +14,15 @@ default_args = {
 
 @dag(
     default_args=default_args,
-    dag_id='bq_fore_to_parq_v01',
+    dag_id='bq_sun_to_parq_v01',
     description=
-    'Save forecast staging data from BQ as a backup local parquet file.',
+    'Save sunrise/set staging data from BQ as a backup local parquet file.',
     start_date=datetime(2024, 7, 26),
     schedule_interval='@daily'
 )
-def bq_fore_to_parq():
+def bq_sun_to_parq():
     @task()
-    def save_parq_fore_df_from_bq(backup_name):
+    def save_parq_sun_df_from_bq(backup_name):
         creds = service_account.Credentials.from_service_account_file(
             'keys/mountain-weather-data-f29d5a51ba66.json'
         )
@@ -30,12 +30,12 @@ def bq_fore_to_parq():
         pandas_gbq.context.project = 'mountain-weather-data'
 
         df = pandas_gbq.read_gbq(
-            'SELECT * FROM `mountain-weather-data.from_airflow.forecast_staging`',
+            'SELECT * FROM `mountain-weather-data.from_airflow.sun_staging`',
             project_id='mountain-weather-data'
         )
         df.to_parquet(backup_name)
 
-    save_parq_fore_df_from_bq('backups/airflow_forecast_staging.parquet')
+    save_parq_sun_df_from_bq('backups/airflow_sun_staging.parquet')
 
 
-dag = bq_fore_to_parq()
+dag = bq_sun_to_parq()
